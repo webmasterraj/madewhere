9eb9cb155a36daebf15ad30a75b03cc9ada69fa1
Made Where <madewherehelp@gmail.com>
2025-02-05 15:18:06 +0530

    new tests wip

--- js/background.js
+++ js/background.js
@@ -2,91 +2,165 @@ importScripts("../lib/CStorage.js");
 importScripts("../lib/country.js");
 
 // sites to check for country
-const amazonSites = ["www.amazon.ca","www.amazon.in", "www.amazon.com"];
+const amazonSites = ["www.amazon.com", "www.amazon.ca","www.amazon.co.uk", "www.amazon.in"];
 
-async function getCountry(asin, host) 
-{
-	let savedCountry = await CSGet(asin);
+// page sections configuration
+const pageSections = [
+    {
+        name: "detailBullets",
+        regex: /<div id="detailBulletsWrapper_feature_div"[\s\S]+?<\/div>/g,
+        extractFn: (html) => {
+            const a = html.substr(html.toLowerCase().indexOf("country of origin") + "country of origin".length);
+            const b = a.substr(0, a.indexOf("</li>"));
+            return b.match(/(?<=<span>).+?(?=<\/span>)/g)?.at(0);
+        }
+    },
+    {
+        name: "productDetails",
+        regex: /<table id="productDetails_detailBullets_sections1"[\s\S]+?<\/table>/g,
+        extractFn: (html) => {
+            const a = html.substr(html.toLowerCase().indexOf("country of origin") + "country of origin".length);
+            const b = a.substr(0, a.indexOf("</tr>"));
+            return b.match(/(?<=<td[\s\S]+>)[\s\S]+?(?=<\/td>)/g)?.at(0);
+        }
+    },
+    {
+        name: "techSpecs",
+        regex: /<table id="productDetails_techSpec_section_1"[\s\S]+?<\/table>/g,
+        extractFn: (html) => {
+            const a = html.substr(html.toLowerCase().indexOf("country of origin") + "country of origin".length);
+            const b = a.substr(0, a.indexOf("</tr>"));
+            return b.match(/(?<=<td[\s\S]+>)[\s\S]+?(?=<\/td>)/g)?.at(0);
+        }
+    }
+];
 
-	if (savedCountry === undefined) 
-	{
-		let pageDatas = await Promise.allSettled(
-			amazonSites.map(async amazonSite=>{
-				return new Promise(async (resolve, reject)=>{
-					try
-					{
-						let data = {};
-						data.pageHtml = await (await fetch(`https://${amazonSite}/dp/${asin}`, {credentials:"include",headers:{"same-cookies":"true"}})).text();
-						data.host = amazonSite;
-						resolve(data);
-					}
-					catch (error)
-					{
-						// console.log("There was an error...", error);
-						resolve(null);
-					}
-				});
-			})
-		);
+async function checkSite(asin, site) {
+    try {
+        const pageHtml = await fetch(`https://${site}/dp/${asin}`).then(r => r.text());
+        
+        // Initialize source info
+        let sourceInfo = {
+            domain: site,
+            url: `https://${site}/dp/${asin}`,
+            section: null,
+            text: null,
+            country: null,
+            timestamp: new Date().toISOString()
+        };
 
-		pageDatas = pageDatas.filter(pageData=>pageData?.value?.pageHtml); // remove null values
+        const countryOfOriginRegex = /[Oo]f [Oo]rigin/;
+        
+        for (const {name, regex, extractFn} of pageSections) {
+            const html = pageHtml.match(regex)?.at(0);
+            if (html && countryOfOriginRegex.test(html)) {
+                const extractedText = extractFn(html);
+                if (extractedText) {
+                    // Clean the extracted text
+                    const cleanText = extractedText
+                        .replace(/[\u{0080}-\u{FFFF}]/gu, "")
+                        .replace(/&.+;/g, "")
+                        .trim();
+                    
+                    const country = getCountryFromText(cleanText);
+                    if (country) {
+                        sourceInfo = {
+                            ...sourceInfo,
+                            section: name,
+                            text: cleanText,
+                            country: country
+                        };
+                        break;
+                    }
+                }
+            }
+        }
 
-		pageDatas.sort((a, b) => (a.value.host === host ? -1 : b.value.host === host ? 1 : 0)); // bringing the current host to the top
+        return sourceInfo;
 
-		for (let pageData of pageDatas) 
-		{
-			try
-			{
-				let pageHtml = pageData.value.pageHtml;
+    } catch (error) {
+        console.error(`Error fetching ${site}:`, error);
+        return {
+            domain: site,
+            url: `https://${site}/dp/${asin}`,
+            section: null,
+            text: null,
+            country: null,
+            timestamp: new Date().toISOString()
+        };
+    }
+}
 
-				// page sections where country of origin is found
-				const detailBulletsHtml = pageHtml.match(/<div id="detailBulletsWrapper_feature_div"[\s\S]+?<\/div>/g)?.at(0);
-				const prodDetailsHtml = pageHtml.match(/<table id="productDetails_detailBullets_sections1"[\s\S]+?<\/table>/g)?.at(0);
-				const techSpecHtml = pageHtml.match(/<table id="productDetails_techSpec_section_1"[\s\S]+?<\/table>/g)?.at(0);
-				
-				// regex to check if 'country of origin' text is present in the page section
-				const countryOfOriginRegex = /[Oo]f [Oo]rigin/;
+function handleCountryConflict(result, host) {
+    const foundSources = result.sources.filter(s => s.country);
+    const uniqueCountries = new Set(foundSources.map(s => s.country));
+    
+    // Set conflict flag if we found different countries
+    result.hasConflict = uniqueCountries.size > 1;
+    
+    if (!result.hasConflict) {
+        // No conflict - use the only country we found
+        const countryName = foundSources[0].country;
+        result.country = {
+            name: countryName,
+            code: getCountryCode(countryName),
+            flag: getFlagUrl(getCountryCode(countryName))
+        };
+    } else {
+        // We have a conflict - choose based on priority
+        let selectedSource;
+        
+        if (host) {
+            // Try to find country from current host first
+            selectedSource = foundSources.find(s => s.domain === host);
+        }
+        
+        if (!selectedSource) {
+            // Go through amazonSites in order
+            for (const site of amazonSites) {
+                selectedSource = foundSources.find(s => s.domain === site);
+                if (selectedSource) break;
+            }
+        }
 
-				// Check page source to get a better idea of what this code is doing
-				if (detailBulletsHtml && countryOfOriginRegex.test(detailBulletsHtml)) 
-				{
-					let a = detailBulletsHtml.substr(detailBulletsHtml.toLowerCase().indexOf("country of origin") + "country of origin".length);
-					let b = a.substr(0, a.indexOf("</li>"));
-					savedCountry = b.match(/(?<=<span>).+?(?=<\/span>)/g)?.at(0);
-				}
-				else if (prodDetailsHtml && countryOfOriginRegex.test(prodDetailsHtml))
-				{
-					let a = prodDetailsHtml.substr(prodDetailsHtml.toLowerCase().indexOf("country of origin") + "country of origin".length);
-					let b = a.substr(0, a.indexOf("</tr>"));
-					savedCountry = b.match(/(?<=<td[\s\S]+>)[\s\S]+?(?=<\/td>)/g)?.at(0);
-				}
-				else if (techSpecHtml && countryOfOriginRegex.test(techSpecHtml)) 
-				{
-					let a = techSpecHtml.substr(techSpecHtml.toLowerCase().indexOf("country of origin") + "country of origin".length);
-					let b = a.substr(0, a.indexOf("</tr>"));
-					savedCountry = b.match(/(?<=<td[\s\S]+>)[\s\S]+?(?=<\/td>)/g)?.at(0);
-				}
-				else continue;
-				
-				if(savedCountry) 
-				{
-					savedCountry = savedCountry.replace(/[\u{0080}-\u{FFFF}]/gu, "").replace(/&.+;/g, "").trim(); // removing any useless unicode characters
-					
-					CSSet(asin, savedCountry);
+        // Set the country from our selected source
+        if (selectedSource) {
+            const countryName = selectedSource.country;
+            result.country = {
+                name: countryName,
+                code: getCountryCode(countryName),
+                flag: getFlagUrl(getCountryCode(countryName))
+            };
+        }
+    }
+}
 
-					return {asin, country: CountryData.find(x => x.name.toLowerCase() == savedCountry?.toLowerCase())};
-				}
-			}
-			catch (error)
-			{
-				console.log("There was an error getting product info from", pageHtml, "for", asin, error);
-			}
-		}
+async function getCountry(asin, host) {
+    // Check storage first
+    let savedData = await CSGet(asin);
+    if (savedData?.sources?.length > 0) {
+        return savedData;
+    }
 
-		CSSet(asin, null); // no country was found for this asin
-	}
+    // Initialize result structure
+    const result = {
+        country: undefined,
+        sources: [],
+        hasConflict: false
+    };
 
-	return {asin, country: CountryData.find(x => x.name.toLowerCase() == savedCountry?.toLowerCase())};
+    // Check all sites in parallel
+    result.sources = await Promise.all(
+        amazonSites.map(site => checkSite(asin, site))
+    );
+
+    // Handle conflicts and set the country
+    handleCountryConflict(result, host);
+
+    // Save to storage
+    await CSSet(asin, result);
+    
+    return result;
 }
 
 // For testing to see if the code can get country of origin from different sections of each website
@@ -97,32 +171,84 @@ async function getCountry(asin, host)
     const testCases = [
         {
             asin: "B081H3Y5NW",
-            description: "Product showing different countries on different sites",
-            expectedResults: {
-                "www.amazon.com": "China",
-                "www.amazon.in": "Vietnam"
-            }
+            description: "Product showing different countries (testing conflict resolution)",
+            expectedSources: {
+                "www.amazon.com": {
+                    country: "China",
+                    section: "detailBullets",
+                    text: "Country of Origin: China"
+                },
+                "www.amazon.in": {
+                    country: null,
+                    section: null,
+                    text: null,
+                },
+                "www.amazon.ca": {
+                    country: "Vietnam",
+                    section: "detailBullets",
+                    text: "Country of Origin: Vietnam"
+                },
+                "www.amazon.co.uk": {
+                    country: null,
+                    section: null,
+                    text: null
+                }
+            },
+            hasConflict: true
         },
         {
             asin: "B01B0ADMP8",
             description: "Product with country in detailBullets",
-            expectedResults: {
-                "www.amazon.com": "USA"
-            }
+            expectedSources: {
+                "www.amazon.com": {
+                    country: "USA",
+                    section: "detailBullets",
+                    text: "Country of Origin: USA"
+                },
+                "www.amazon.in": {
+                    country: "USA",
+                    section: "techSpecs",
+                    text: "Country of Origin: USA"
+                },
+                "www.amazon.ca": {
+                    country: null,
+                    section: null,
+                    text: null
+                },
+                "www.amazon.co.uk": {
+                    country: null,
+                    section: null,
+                    text: null
+                }
+            },
+            hasConflict: false
         },
         {
             asin: "B007RM3010",
             description: "Product with country in detailBullets on .in",
-            expectedResults: {
-                "www.amazon.in": "United Kingdom"
-            }
-        },
-        {
-            asin: "B0C811KLKZ",
-            description: "Product with country in techSpecs on .in",
-            expectedResults: {
-                "www.amazon.in": "India"
-            }
+            expectedSources: {
+                "www.amazon.com": {
+                    country: null,
+                    section: null,
+                    text: null
+                },
+                "www.amazon.in": {
+                    country: "United Kingdom",
+                    section: "detailBullets",
+                    text: "Country of Origin: United Kingdom"
+                },
+                "www.amazon.ca": {
+                    country: null,
+                    section: null,
+                    text: null
+                },
+                "www.amazon.co.uk": {
+                    country: null,
+                    section: null,
+                    text: null
+                }
+            },
+            hasConflict: false
         }
     ];
     
@@ -132,27 +258,44 @@ async function getCountry(asin, host)
     for (const testCase of testCases) {
         console.log(`\n📦 Testing ${testCase.asin}: ${testCase.description}`);
         
-        for (const [site, expectedCountry] of Object.entries(testCase.expectedResults)) {
-            totalTests++;
+        // Clear storage for this ASIN
+        await CSRemove(testCase.asin);
+        
+        // Run test once - it will check all sources
+        const result = await getCountry(testCase.asin);
+        
+        // Test conflict flag
+        const conflictMatch = result.hasConflict === testCase.hasConflict;
+        totalTests++;
+        passedTests += conflictMatch ? 1 : 0;
+        console.log(`Conflict Flag: ${conflictMatch ? '✅' : '❌'} Expected: ${testCase.hasConflict}, Got: ${result.hasConflict}`);
+        
+        // Test each source
+        for (const [site, expected] of Object.entries(testCase.expectedSources)) {
+            const source = result.sources.find(s => s.domain === site);
+            totalTests += 3; // country, section, text
             
-            // Clear storage only for this ASIN
-            await CSRemove(testCase.asin);
+            // Compare results
+            const countryMatch = source?.country === expected.country;
+            const sectionMatch = source?.section === expected.section;
+            const textMatch = source?.text === expected.text;
             
-            // Run test
-            const result = await getCountry(testCase.asin, site);
-            const actualCountry = result?.country?.name;
+            passedTests += countryMatch ? 1 : 0;
+            passedTests += sectionMatch ? 1 : 0;
+            passedTests += textMatch ? 1 : 0;
             
-            // Compare result
-            const passed = actualCountry === expectedCountry;
-            passedTests += passed ? 1 : 0;
-            
-            // Log result with emoji and colors
+            // Log detailed results for this source
             console.log(
-                `   ${site}: ${passed ? '✅' : '❌'} ` +
-                `Expected: ${expectedCountry}, ` +
-                `Got: ${actualCountry || 'undefined'}`
+                `\n   ${site} (${source?.url}):\n` +
+                `      Country: ${countryMatch ? '✅' : '❌'} Expected: ${expected.country || 'null'}, Got: ${source?.country || 'null'}\n` +
+                `      Section: ${sectionMatch ? '✅' : '❌'} Expected: ${expected.section || 'null'}, Got: ${source?.section || 'null'}\n` +
+                `      Text:    ${textMatch ? '✅' : '❌'} Expected: ${expected.text || 'null'}, Got: ${source?.text || 'null'}`
             );
         }
+        
+        // Log the full result object
+        console.log("\n   Full Result:");
+        console.log("   ", JSON.stringify(result, null, 2).replace(/\n/g, '\n   '));
     }
     
     // Print summary
@@ -162,56 +305,52 @@ async function getCountry(asin, host)
 })();
 
 const contentScript = {
-	id: 'amazonFlags',
-	matches: ['https://www.amazon.com/*'],
-	js: [
-		"lib/jquery-3.6.0.min.js",
-		"js/contentScript.js"
-	],
-	css:["css/contentScript.css"],
-	runAt: 'document_end',
+    id: 'amazonFlags',
+    matches: ['https://www.amazon.com/*'],
+    js: ['lib/CStorage.js', 'lib/country.js', 'js/content.js'],
+    runAt: 'document_end'
 };
 
 // Set up content script at install time
 chrome.runtime.onInstalled.addListener(function () 
 {
-	chrome.scripting.registerContentScripts([contentScript]);
-	chrome.storage.local.clear();
-	chrome.storage.sync.set({ enabled: true });
-	console.log("Made in Where Installed.");
+    chrome.scripting.registerContentScripts([contentScript]);
+    chrome.storage.local.clear();
+    chrome.storage.sync.set({ enabled: true });
+    console.log("Made in Where Installed.");
 });
 
-
+// Listen for messages from content script
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) 
 {
-	if (request.action == "getCountryInfos") 
-	{
-		(async () => {
-			try 
-			{
-				let countries = await Promise.allSettled(request.asins.map(asin=>getCountry(asin, request.host)));
-				sendResponse(countries.map(c=>c.value));
-			}
-			catch (error) 
-			{
-				sendResponse(error);
-			}
-		})();
+    if (request.action == "getCountryInfos") 
+    {
+        (async () => {
+            try 
+            {
+                let countries = await Promise.allSettled(request.asins.map(asin=>getCountry(asin, request.host)));
+                sendResponse(countries.map(c=>c.value));
+            }
+            catch (error) 
+            {
+                sendResponse(error);
+            }
+        })();
 
-		return true;
-	}
-	else if (request.action == "toggle") 
-	{
-		if(request.enabled ?? true)
-		{
-			chrome.scripting.registerContentScripts([contentScript]);
-			console.log("Script Enabled.");
-		}
-		else
-		{
-			chrome.scripting.unregisterContentScripts({ids: [contentScript.id]});
-			console.log("Script Disabled.");
-		}
-		chrome.storage.sync.set({ enabled: (request.enabled ?? true) });
-	}
+        return true;
+    }
+    else if (request.action == "toggle") 
+    {
+        if(request.enabled ?? true)
+        {
+            chrome.scripting.registerContentScripts([contentScript]);
+            console.log("Script Enabled.");
+        }
+        else
+        {
+            chrome.scripting.unregisterContentScripts({ids: [contentScript.id]});
+            console.log("Script Disabled.");
+        }
+        chrome.storage.sync.set({ enabled: (request.enabled ?? true) });
+    }
 });
